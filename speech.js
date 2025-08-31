8835f244132c72745d2cf193b7bbf2c5af2925af
hiteshSingh57 <hiteshsingh001361@gmail.com>
2025-08-31 13:21:13 +0530

    add new files into speak to me repository

---
+++ speech.js
@@ -0,0 +1,37 @@
+// Speech Recognition Setup
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.lang = 'en-US';
+
+    let finalTranscript = '';
+
+    recognition.onresult = function(event) {
+      let interimTranscript = '';
+      for (let i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          finalTranscript += event.results[i][0].transcript + ' ';
+        } else {
+          interimTranscript += event.results[i][0].transcript;
+        }
+      }
+      document.getElementById('textArea').value = finalTranscript + interimTranscript;
+    };
+
+    function startRecognition() {
+      finalTranscript = '';
+      recognition.start();
+    }
+
+    function stopRecognition() {
+      recognition.stop();
+    }
+
+    // Text to Speech
+    function speakText() {
+      const text = document.getElementById('textArea').value;
+      const utterance = new SpeechSynthesisUtterance(text);
+      utterance.lang = 'en-US';
+      window.speechSynthesis.speak(utterance);
+    }
